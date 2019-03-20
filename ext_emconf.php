8170525bb4773fba30d6d0f7083558fd7bb447a4
Maik Schneider <m.schneider@blueways.de>
2019-03-20 12:22:43 +0100

    chore: set version to 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,5 +23,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Maik Schneider',
     'author_email' => 'm.schneider@blueways.de',
     'author_company' => 'blueways',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
 ];

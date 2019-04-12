e935e2ec802426cb4d8aea196c80aeabd295f917
Maik Schneider <m.schneider@blueways.de>
2019-04-12 11:24:54 +0200

    feat: include typoscript setup in template

--- Classes/Controller/TemplateController.php
+++ Classes/Controller/TemplateController.php
@@ -2,6 +2,8 @@
 
 namespace Blueways\BwStaticTemplate\Controller;
 
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+
 class TemplateController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
 
@@ -25,7 +27,6 @@ class TemplateController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
                 $fileReference = $resourceFactory->getFileReferenceObject($itemUid);
                 array_push($files, $fileReference);
             }
-
         }
 
         $json = $this->settings['json'];
@@ -34,7 +35,27 @@ class TemplateController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
             $this->view->assignMultiple((array)$data);
         }
 
+        $configurationManager = $this->objectManager->get(ConfigurationManager::class);
+        $typoscript = $configurationManager->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+        $typoscript = $this->replaceKeyDots($typoscript);
+
+        $this->view->assign('typoscript', $typoscript);
         $this->view->assign('files', $files);
         $this->view->setTemplatePathAndFilename($this->settings['templateName']);
+    }
+
+    /**
+     * @param $arr
+     * @return mixed
+     */
+    private function replaceKeyDots($arr)
+    {
+        foreach ($arr as $key => $item) {
+            if (is_array($item) && substr($key, -1) === '.') {
+                $arr[substr($key, 0, -1)] = $this->replaceKeyDots($item);
+                unset($arr[$key]);
+            }
+        }
+        return $arr;
     }
 }

031bc2eb35a82cfde51fff031433629017b7f87f
Maik Schneider <m.schneider@blueways.de>
2021-10-29 05:30:39 +0200

    feat: switch to custom content element instead of plugin

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -15,3 +15,40 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['bwstatictemp
     'bwstatictemplate_pi1',
     'FILE:EXT:bw_static_template/Configuration/FlexForms/' . $flexformName
 );
+
+$GLOBALS['TCA']['tt_content']['types']['bw_static_template'] = [
+    'showitem' => '
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            header,
+            bodytext,
+            assets,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+            --palette--;;frames,
+            --palette--;;appearanceLinks,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+            --palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+            categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+            rowDescription,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+    ',
+    'columnsOverrides' => [
+        'header' => [
+            'label' => 'LLL:EXT:bw_static_template/Resources/Private/Language/locallang.xlf:template',
+            'config' => [
+                'eval' => 'trim,required'
+            ]
+        ],
+        'bodytext' => [
+            'label' => 'LLL:EXT:bw_static_template/Resources/Private/Language/locallang.xlf:json',
+            'config' => [
+                'renderType' => 'jsonForm',
+            ],
+        ],
+    ]
+];

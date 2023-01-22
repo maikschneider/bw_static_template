477b7564d234ac2811d61a560e8a0dce54e7fad7
Maik Schneider <mas@xima.de>
2023-01-22 10:53:45 +0100

    feat: use v12 as dev dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,8 +6,8 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-11.99.99',
-            'bw_jsoneditor' => '1.0.0-1.99.99',
+            'typo3' => '8.7.0-12.99.99',
+            'bw_jsoneditor' => '1.1.0-1.99.99',
         ],
         'conflicts' => [
         ],

b8e22c8311b3d770ac3dff2354d1c2b30858943b
Maik Schneider <mas@xima.de>
2023-01-22 19:12:02 +0100

    feat: allow relative template names

---
+++ Classes/UserFunc/ContentElementUserFunc.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Blueways\BwStaticTemplate\UserFunc;
+
+class ContentElementUserFunc
+{
+    public function getTemplateRootPath(string $content): string
+    {
+        if (strpos($content, 'EXT:') === 0) {
+            $pathSegments = explode('/', $content) ?: [];
+            array_pop($pathSegments);
+            return implode('/', $pathSegments);
+        }
+        return '';
+    }
+
+    public function getTemplateName(string $content): string
+    {
+        if (strpos($content, 'EXT:') === 0) {
+            $pathSegments = explode('/', $content) ?: [];
+            return array_pop($pathSegments);
+        }
+        return $content;
+    }
+
+}

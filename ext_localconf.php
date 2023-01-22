163749512e87a309254217dd511e91eebe47a3f5
Maik Schneider <mas@xima.de>
2023-01-22 12:54:34 +0100

    fix: remove TYPO3_MODE check

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,5 @@
 <?php
 
-defined('TYPO3_MODE') || die();
-
 $typo3Version = TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionStringToArray(TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version());
 $controllerName = $typo3Version['version_main'] >= 10 ? \Blueways\BwStaticTemplate\Controller\TemplateController::class : 'Template';
 $extensionName = $typo3Version['version_main'] >= 10 ? 'BwStaticTemplate' : 'Blueways.BwStaticTemplate';

8b4d138e72b5d4ac7423f6958cbf831c1fbea6a0
Maik Schneider <schneider.maik@me.com>
2023-03-29 20:56:20 +0200

    feat: include TypoScript globally, remove old flexforms, remove static template includes

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,14 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['bwStaticTemplate_v3UpgradeWizard'] = \Blueways\BwStaticTemplate\Upgrades\V3UpgradeWizard::class;
+defined('TYPO3') or die();
+
+call_user_func(function () {
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['bwStaticTemplate_v3UpgradeWizard'] = \Blueways\BwStaticTemplate\Upgrades\V3UpgradeWizard::class;
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
+        'bw_static_template',
+        'setup',
+        "@import 'EXT:bw_static_template/Configuration/TypoScript/setup.typoscript'"
+    );
+});

a6404c59b37936b5154e992867140de80ea42c1e
Maik Schneider <schneider.maik@me.com>
2023-03-29 20:58:12 +0200

    fix: remove old icon registration, fix icon path

--- ext_tables.php
+++ ext_tables.php
@@ -1,15 +1,9 @@
 <?php
 
+defined('TYPO3') or die();
+
 call_user_func(
     function () {
-        // Register icon
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-        $iconRegistry->registerIcon(
-            'tx_bwstatictemplate_pi1',
-            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-            ['source' => 'EXT:bw_static_template/Resources/Public/Icons/tt_content_pi1.svg']
-        );
-
         // register css
         $GLOBALS['TBE_STYLES']['skins']['bw_static_template']['stylesheetDirectories'][] = 'EXT:bw_static_template/Resources/Public/Css/';
     }

39637995acf1395a7992f05d048ac5d6321f072d
Maik Schneider <mas@xima.de>
2024-11-07 18:50:30 +0100

    feat: dark mode adjustments

--- ext_tables.php
+++ ext_tables.php
@@ -5,6 +5,6 @@ defined('TYPO3') or die();
 call_user_func(
     function () {
         // register css
-        $GLOBALS['TBE_STYLES']['skins']['bw_static_template']['stylesheetDirectories'][] = 'EXT:bw_static_template/Resources/Public/Css/';
+        $GLOBALS['TYPO3_CONF_VARS']['BE']['stylesheets']['bw_static_template'] = 'EXT:bw_static_template/Resources/Public/Css/Backend.css';
     }
 );

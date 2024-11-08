48fad7ab13bc2215921b4496bc14dd2500d44911
Maik Schneider <mas@xima.de>
2024-11-08 16:20:33 +0100

    feat: always include page.tsconfig

---
+++ ext_localconf.php
@@ -0,0 +1,12 @@
+<?php
+
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+$versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+if ($versionInformation->getMajorVersion() < 12) {
+    ExtensionManagementUtility::addPageTSConfig('
+      @import "EXT:bw_static_template/Configuration/page.tsconfig"
+   ');
+}

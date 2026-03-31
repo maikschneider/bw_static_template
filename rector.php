86932639f83d5e03f49256806bea936e94e0d04f
Maik Schneider <maik.schneider@xima.de>
2026-03-31 20:56:36 +0200

    fix: update dev dependencies for TYPO3 v12/v13 compat
    - Allow phpstan-typo3 ^2.1 and typo3-rector ^3.13
    - Remove bootstrap-package and nikic/php-parser overrides
    - Downgrade rector set to UP_TO_TYPO3_12
    - Remove ConvertImplicitVariablesToExplicitGlobalsRector rule

--- rector.php
+++ rector.php
@@ -26,15 +26,14 @@ return RectorConfig::configure()
     ->withSets([
         Typo3SetList::CODE_QUALITY,
         Typo3SetList::GENERAL,
-        Typo3LevelSetList::UP_TO_TYPO3_13,
+        Typo3LevelSetList::UP_TO_TYPO3_12,
     ])
     // To have a better analysis from PHPStan, we teach it here some more things
     ->withPHPStanConfigs([
         Typo3Option::PHPSTAN_FOR_RECTOR_PATH,
     ])
     ->withRules([
-        AddVoidReturnTypeWhereNoReturnRector::class,
-        ConvertImplicitVariablesToExplicitGlobalsRector::class,
+        AddVoidReturnTypeWhereNoReturnRector::class
     ])
     ->withConfiguredRule(ExtEmConfRector::class, [
         ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.1.0-8.3.99',

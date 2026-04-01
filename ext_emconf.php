dc494104c8ef04346485f7d0bb2942dbf6e10179
Maik Schneider <maik.schneider@xima.de>
2026-04-01 10:19:23 +0200

    feat: drop typo3 v12 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-14.3.99',
+            'typo3' => '13.4.0-14.3.99',
             'bw_jsoneditor' => '2.0.0-2.99.99',
         ],
         'conflicts' => [

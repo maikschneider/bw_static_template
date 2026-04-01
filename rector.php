7643471404c8f7877cd7da8fc0bcaff375d405d2
Maik Schneider <maik.schneider@xima.de>
2026-04-01 11:49:57 +0200

    fix: use php8.2 as min version constraint in rector config

--- rector.php
+++ rector.php
@@ -34,7 +34,7 @@ return RectorConfig::configure()
         AddVoidReturnTypeWhereNoReturnRector::class,
     ])
     ->withConfiguredRule(ExtEmConfRector::class, [
-        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.1.0-8.4.99',
+        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.2.0-8.4.99',
         ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '13.4.0-14.3.99',
         ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
     ]);
